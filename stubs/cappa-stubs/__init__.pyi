fdee41d665ecaec70bdd61ac0a27919a09d54811
blakeNaccarato <blake.naccarato@gmail.com>
2026-02-20 10:44:19 -0800

    Remove cappa

--- stubs/cappa-stubs/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-from cappa.base import command as command
-from cappa.base import invoke as invoke
-
-def __getattr__(name: str) -> Any: ...  # pyright: ignore[reportIncompleteStub]
-
